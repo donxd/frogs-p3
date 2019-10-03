c23eb1fe46bb82d75dd11d43c034a7b9fb512aed
donxd <n0s3.xd@gmail.com>
2019-10-03 17:57:53 -0500

    fix(frogs-p3): changes on building process

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@ rm -rf build/
 polymer build
 mv build/deploy ./deploy
 rm -rf build/
-mv node_modules node_modules.bak
 git checkout gh-pages
-rm -rf images/ node_modules/ frogs-p3.js index.html package.json
+rm -rf images/ index.html
 mv deploy/* ./
 rm -rf deploy/
 mv index-deploy.html index.html
